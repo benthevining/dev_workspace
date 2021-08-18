96b6265abfb4a9b953f593860ce52f85e60dafd8
Ben Vining <ben.the.vining@gmail.com>
2021-08-18 07:11:35 -0700

    updating

--- rake/RakeUtils.rb
+++ rake/RakeUtils.rb
@@ -4,9 +4,15 @@ end
 
 
 require_relative "constants.rb"
+
 require_relative "os.rb"
 require_relative "files.rb"
+
 require_relative "git.rb"
 require_relative "clangformat.rb"
+
+require_relative "default_gitignore/gitignore.rb"
+require_relative "github_scripts/github_scripts.rb"
+
 require_relative "cmake.rb"
 require_relative "init.rb"
