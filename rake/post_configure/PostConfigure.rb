baa694a9f34ffbf960ebc68bc95da20c214c7888
Ben Vining <ben.the.vining@gmail.com>
2021-08-18 22:46:18 -0700

    updating

--- rake/post_configure/PostConfigure.rb
+++ rake/post_configure/PostConfigure.rb
@@ -2,6 +2,7 @@ require_relative "DefaultGithubRepo/RepoUtils.rb"
 
 require_relative "gitignore.rb"
 require_relative "clangformat.rb"
+require_relative "github_scripts.rb"
 
 module PostConfig
 
---
+++ rake/post_configure/github_scripts.rb
@@ -0,0 +1,15 @@
+module GithubScripts
+
+	def self.update()
+
+		REPO_SUBDIRS.each { |subdir|
+
+			sub_dir = strip_array_foreach_chars(subdir)
+			next if sub_dir.empty?
+
+			dir = REPO_ROOT + "/" + sub_dir
+
+			GithubScriptsHandle.update_subdir(dir)
+		}
+	end
+end
