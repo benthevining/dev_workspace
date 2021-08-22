fa505ba085f1cb2d2e37edea2502ef284cee23ca
Ben Vining <ben.the.vining@gmail.com>
2021-08-22 16:35:37 -0700

    Update zipper.rb

--- rake/zipper.rb
+++ rake/zipper.rb
@@ -3,39 +3,55 @@ module Zipper
 	@@BUILD_DIR = REPO_ROOT + "/Builds"
 
 
-	def self.zip_product(reponame, productName)
+	def self.zip_product(reponame, productName, dirToCopyTo = "")
 
 		dir = @@BUILD_DIR + "/" + reponame
 
-		artefacts = dir + "/" + productName + "_artefacts"
-
 		Dir.chdir(dir) do 
-			Rake.sh ("cmake -E tar cfv " + productName + ".zip --format=zip " + artefacts) do |ok, res| end
+			artefacts = dir + "/" + productName + "_artefacts"
+			zipFileName = productName + ".zip"
+
+			Rake.sh ("cmake -E tar cfv " + zipFileName + " --format=zip " + artefacts) do |ok, res| end
+
+			if not dirToCopyTo.empty?
+				FileUtils.cp(zipFileName, dirToCopyTo)
+			end
 		end
 	end
 
 
-	def self.zip_plugin(productName)
-		self.zip_product(productName.downcase, productName)
+	def self.zip_plugin(productName, dirToCopyTo = "")
+		self.zip_product(productName.downcase, productName, dirToCopyTo)
 	end
 
 
-	def self.zip_app(productName)
+	def self.zip_app(productName, dirToCopyTo = "")
 		if productName == "ImogenRemote"
-			self.zip_product("imogen", productName)
+			self.zip_product("imogen", productName, dirToCopyTo)
 		else 
-			self.zip_product(productName, productName)
+			self.zip_product(productName, productName, dirToCopyTo)
 		end
 	end
 
 
 	def self.zip_all()
+
+		dir = @@BUILD_DIR + "/Artifacts"
+
+		FileUtils.remove_dir(dir) if Dir.exist?(dir)
+
+		Dir.mkdir(dir)
+
 		PLUGIN_NAMES.each { |plugin|
-			self.zip_plugin(plugin)
+			self.zip_plugin(plugin, dir)
 		}
 
 		APP_NAMES.each { |app|
-			self.zip_app(app)
+			self.zip_app(app, dir)
 		}
+
+		Dir.chdir(@@BUILD_DIR) do 
+			Rake.sh ("cmake -E tar cfv Artifacts.zip --format=zip Artifacts") do |ok, res| end
+		end
 	end
 end
