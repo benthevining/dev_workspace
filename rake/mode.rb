91fd68991ccfeac64074bd8542d15698d55c1bb9
Ben Vining <ben.the.vining@gmail.com>
2021-08-22 20:20:32 -0700

    Update mode.rb

--- rake/mode.rb
+++ rake/mode.rb
@@ -1,6 +1,17 @@
 module BuildMode
 
 	def self.parse(input)
+
+		lower = input.downcase
+
+		if lower == "d" or lower == "deb"
+			return "Debug"
+		end
+
+		if lower == "r" or lower == "rel"
+			return "Release"
+		end
+
 		return input.capitalize
 	end
 end
