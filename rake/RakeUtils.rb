a86096d385f6aca601364ceccf973432c452aef5
Ben Vining <ben.the.vining@gmail.com>
2021-09-10 23:38:51 -0500

    updating

--- rake/RakeUtils.rb
+++ rake/RakeUtils.rb
@@ -1,9 +1,11 @@
 require_relative "common/Common.rb"
 
-require_relative "git.rb"
+require_relative "init.rb"
 require_relative "clean.rb"
-require_relative "clangformat.rb"
-require_relative "post_configure/PostConfigure.rb"
 
 require_relative "cmake.rb"
-require_relative "init.rb"
+require_relative "cpack.rb"
+
+require_relative "git.rb"
+require_relative "clangformat.rb"
+require_relative "post_configure/PostConfigure.rb"
--- rake/cpack.rb
+++ rake/cpack.rb
@@ -2,5 +2,8 @@ module CPack
 
 	def self.run(mode)
 
+		Dir.chdir(REPO_ROOT) do 
+			Rake.sh "cpack --config " + REPO_ROOT + "/Builds/CPackConfig.cmake -C " + mode.to_s
+		end
 	end
 end
