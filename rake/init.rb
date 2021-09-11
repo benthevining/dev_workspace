d5cf37f13ee1b239814f1028dbe70db0ee984647
Ben Vining <ben.the.vining@gmail.com>
2021-09-10 23:19:50 -0500

    Rake auto-commit

--- rake/init.rb
+++ rake/init.rb
@@ -1,16 +1,5 @@
 module Init
 
-	def self.init_linux()
-		deps = Array['libasound2-dev', 'libjack-jackd2-dev', 'libcurl4-openssl-dev', 'libfreetype6-dev', 'libx11-dev', 'libxcomposite-dev', 'libxcursor-dev', 'libxcursor-dev', 'libxext-dev', 'libxinerama-dev', 'libxrandr-dev', 'libxrender-dev', 'libwebkit2gtk-4.0-dev', 'libglu1-mesa-dev', 'mesa-common-dev']
-
-		Rake.sh "sudo apt update" do |ok, res| end
-
-		Rake.sh ("sudo apt install " + deps.join(" "))
-
-		Rake.sh "sudo apt install lv2-dev"
-	end
-
-
 	def self.init()
 
 		Dir.chdir(REPO_ROOT) do 
@@ -19,7 +8,15 @@ module Init
 
 		Git.init_all_submodules
 
-		self.init_linux if OS.linux?
+		if OS.linux? 
+
+			deps = Array['libasound2-dev', 'libjack-jackd2-dev', 'libcurl4-openssl-dev', 'libfreetype6-dev', 'libx11-dev', 'libxcomposite-dev', 'libxcursor-dev', 'libxcursor-dev', 'libxext-dev', 'libxinerama-dev', 'libxrandr-dev', 'libxrender-dev', 'libwebkit2gtk-4.0-dev', 'libglu1-mesa-dev', 'mesa-common-dev', 'lv2-dev']
+
+			Rake.sh "sudo apt update" do |ok, res| end
+
+			Rake.sh ("sudo apt install " + deps.join(" "))
+
+		end
 	end
 
 end
