92e7dc6315fd195916024b06a3831a677f069ca4
Ben Vining <ben.the.vining@gmail.com>
2021-09-11 16:21:19 -0500

    Update init.rb

--- rake/init.rb
+++ rake/init.rb
@@ -2,6 +2,8 @@ module Init
 
 	def self.install_ccache()
 
+		puts "\n -- installing ccache..."
+
 		url = "http://github.com/cristianadam/ccache/releases/download/v4.4/" + OS.getNameAsString + ".tar.xz"
 
 		dir = REPO_ROOT + "/Cache/ccache"
@@ -20,6 +22,8 @@ module Init
 
 
 	def self.install_linux_deps
+
+		puts "\n -- installing Linux dependencies..."
 
 		deps = Array['libasound2-dev', 'libjack-jackd2-dev', 'libcurl4-openssl-dev', 'libfreetype6-dev', 'libx11-dev', 'libxcomposite-dev', 'libxcursor-dev', 'libxcursor-dev', 'libxext-dev', 'libxinerama-dev', 'libxrandr-dev', 'libxrender-dev', 'libwebkit2gtk-4.0-dev', 'libglu1-mesa-dev', 'mesa-common-dev', 'lv2-dev']
 
@@ -41,6 +45,8 @@ module Init
 		self.install_ccache
 
 		self.install_linux_deps if OS.linux?
+
+		puts "\n \n"
 	end
 
 end
