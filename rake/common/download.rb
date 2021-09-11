e1e70904491aa3a52abbe616f6dafbd8e9135119
Ben Vining <ben.the.vining@gmail.com>
2021-09-11 16:57:46 -0500

    Update download.rb

--- rake/common/download.rb
+++ rake/common/download.rb
@@ -5,10 +5,12 @@ require "fileutils"
 module Download
 
     def self.download_file(url, path)
-
+        
         if (not File.exist?(path))
             File.new(path, "w")
         end
+
+        # from: https://stackoverflow.com/a/33746205
 
         case io = URI.open(url)
         when StringIO then File.open(path, 'w') { |f| f.write(io.read) }
