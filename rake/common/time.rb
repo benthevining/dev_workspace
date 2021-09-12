fe385f75d29ac5c1684c5675d74df6fc6e69c0d9
Ben Vining <ben.the.vining@gmail.com>
2021-09-12 02:22:53 -0500

    Update time.rb

--- rake/common/time.rb
+++ rake/common/time.rb
@@ -1,11 +1,17 @@
 module FormattedTime
 
+	def self.check_time_token(token)
+		return "0" + token.to_s if (token.to_s.length == 1)
+		return token
+	end
+
+
 	def self.get()
 		timeNow = Time.now
 
-		hr  = timeNow.hour.to_s; hr = "0" + hr  if (hr.length == 1)
-		min = timeNow.min.to_s; min = "0" + min if (min.length == 1)
-		sec = timeNow.sec.to_s; sec = "0" + sec if (sec.length == 1)
+		hr  = timeNow.hour.to_s; hr = self.check_time_token(hr)
+		min = timeNow.min.to_s; min = self.check_time_token(min)
+		sec = timeNow.sec.to_s; sec = self.check_time_token(sec)
 
 		return hr.to_s + ":" + min.to_s + ":" + sec.to_s
 	end
@@ -16,9 +22,9 @@ module FormattedTime
 		startTokens = startString.split(":")
 		endTokens = endString.split(":")
 
-		hr  = (endTokens[0].to_i - startTokens[0].to_i).to_s; hr  = "0" + hr  if (hr.length == 1)
-		min = (endTokens[1].to_i - startTokens[1].to_i).to_s; min = "0" + min if (min.length == 1)
-		sec = (endTokens[2].to_i - startTokens[2].to_i).to_s; sec = "0" + sec if (sec.length == 1)
+		hr  = (endTokens[0].to_i - startTokens[0].to_i).to_s; hr  = self.check_time_token(hr)
+		min = (endTokens[1].to_i - startTokens[1].to_i).to_s; min = self.check_time_token(min)
+		sec = (endTokens[2].to_i - startTokens[2].to_i).to_s; sec = self.check_time_token(sec)
 
 		return hr.to_s + ":" + min.to_s + ":" + sec.to_s
 	end
