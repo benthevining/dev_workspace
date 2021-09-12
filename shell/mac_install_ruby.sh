01695eec9b14b6bf00fa6e3c0ae0ffecfdf63237
Ben Vining <ben.the.vining@gmail.com>
2021-09-12 03:29:19 -0500

    Update mac_install_ruby.sh

--- shell/mac_install_ruby.sh
+++ shell/mac_install_ruby.sh
@@ -1,5 +1,13 @@
 #!bin/bash
 
-cd ~
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+WORKING_DIR+="/../Cache/install_ruby"
+
+if [ ! -d "$WORKING_DIR" ]; then  
+	mkdir $WORKING_DIR
+fi
+
+cd $WORKING_DIR
 curl --remote-name https://raw.githubusercontent.com/monfresh/install-ruby-on-macos/master/install-ruby
-/usr/bin/env bash install-ruby 2>&1 | tee ~/laptop.log
+/usr/bin/env bash $WORKING_DIR/install-ruby 2>&1 | tee $WORKING_DIR/install.log
