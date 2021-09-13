f105c9465fea7ce54002b63bc798d00879d0f519
Ben Vining <ben.the.vining@gmail.com>
2021-09-13 14:07:16 -0500

    reorganizing rake scripts

--- ruby/RubyUtils.rb
+++ ruby/RubyUtils.rb
@@ -1,8 +1,3 @@
 require_relative "common/Common.rb"
 require_relative "program_wrappers/ProgramWrappers.rb"
-
-require_relative "init.rb"
-require_relative "clean.rb"
-require_relative "default_files.rb"
-
-require_relative "log.rb"
+require_relative "modules/Modules.rb"
---
+++ ruby/modules/Modules.rb
@@ -0,0 +1,4 @@
+require_relative "clean.rb"
+require_relative "default_files.rb"
+require_relative "init.rb"
+require_relative "log.rb"
