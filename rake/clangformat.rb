0d11a32538fee591224c41fb26b4a21097141ed0
Ben Vining <ben.the.vining@gmail.com>
2021-09-13 13:02:46 -0500

    updating

--- rake/clangformat.rb
+++ rake/clangformat.rb
@@ -1,9 +1,42 @@
 module ClangFormat 
 
-	def self.run()
+	def self.process_dir(dir)
 
+		Dir.chdir(dir) do
+			Rake.sh "clang-format -i *.c *.h *.cpp *.hpp" do |ok, res| end
+
+			Dir.glob("**/").each do |file|
+
+				if dir[-1] == "/"
+					newDir = dir + file
+				else 
+					newDir = dir + "/" + file
+				end
+
+				newDir.slice!(newDir.length-1, newDir.length) if newDir[-1] == "/"
+
+				self.process_dir(newDir) if Dir.exist?(newDir)
+			end
+		end
+	end
+
+
+	def self.run()
 		REPO_PATHS.each { |dir|
-			ClangFormatHandle.process_dir(dir)
+			self.process_dir(dir)
 		}
+	end
+
+
+	def self.configure_repo(dir)
+
+		to_clangformat_path = -> (basedir) {
+			return basedir.to_s + "/.clang-format"
+		}
+
+		# copy the clang-format config file from the Workspace repo to all child repos
+		Dir.chdir(dir) do 
+			FileUtils.cp(to_clangformat_path.(REPO_ROOT), to_clangformat_path.(dir))
+		end
 	end
 end
--- rake/post_configure/PostConfigure.rb
+++ rake/post_configure/PostConfigure.rb
@@ -3,11 +3,9 @@ require_relative "DefaultGithubRepo/RepoUtils.rb"
 module PostConfig
 
 	def self.run()
-
 		REPO_PATHS.each { |dir|
 			RepoUtils.configure(dir)
+			ClangFormat.configure_repo(REPO_ROOT)
 		}
-
-		RepoUtils.configure(REPO_ROOT + "/lab")
 	end
 end
