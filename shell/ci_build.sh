89ca1e14f438bf77687696815e0dbe4dfd6b6706
Ben Vining <ben.the.vining@gmail.com>
2021-09-14 21:58:35 -0500

    Update ci_build.sh

--- shell/ci_build.sh
+++ shell/ci_build.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
-# options
+################
 
 BUILD_CONFIG="release"
+
 export BV_COMMIT_TO_REPOS="false"
 export BV_DEFAULT_BUILD_CONFIG=$BUILD_CONFIG
 
-# dir
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+################
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd $SCRIPT_DIR/..
 
-#
+################
 
 rake init
 
 rake "config[$BUILD_CONFIG]"
 
 rake "build:all[$BUILD_CONFIG]"
-
-#
 
 exit 0
