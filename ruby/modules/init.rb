27d346ad1a8923ad125793375ec088939fe82982
Ben Vining <ben.the.vining@gmail.com>
2021-09-15 07:28:16 -0500

    updating

--- ruby/modules/init.rb
+++ ruby/modules/init.rb
@@ -60,9 +60,9 @@ module Init
 
 		deps = Array['libasound2-dev', 'libjack-jackd2-dev', 'libcurl4-openssl-dev', 'libfreetype6-dev', 'libx11-dev', 'libxcomposite-dev', 'libxcursor-dev', 'libxcursor-dev', 'libxext-dev', 'libxinerama-dev', 'libxrandr-dev', 'libxrender-dev', 'libwebkit2gtk-4.0-dev', 'libglu1-mesa-dev', 'mesa-common-dev', 'lv2-dev']
 
-		Rake.sh "apt-get update" do |ok, res| end
+		Rake.sh "sudo apt-get update" do |ok, res| end
 
-		Rake.sh ("apt-get install -y " + deps.join(" "))
+		Rake.sh ("sudo apt-get install -y " + deps.join(" "))
 	end
 
 
