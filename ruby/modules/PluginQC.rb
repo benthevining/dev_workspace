604856e52e52d4b8b25b3d83cc01ec6850212559
Ben Vining <ben.the.vining@gmail.com>
2021-09-18 22:25:56 -0500

    Create PluginQC.rb

---
+++ ruby/modules/PluginQC.rb
@@ -0,0 +1,27 @@
+module PluginQC
+
+	def self.build(mode = "Debug")
+
+		Dir.chdir(REPO_ROOT + "/plugin_qc") do 
+
+			puts "\nBuilding plugin QC tools...\n"
+
+			# configure cmake
+
+			command = "cmake -B Builds -DCMAKE_BUILD_TYPE=" + mode
+
+			if OS.mac?
+				command += " -G Xcode"
+			elsif OS.windows?
+				command += " -G \"Visual Studio 16 2019\""
+			end # use default Ninja generator on Linux
+
+			Rake.sh command
+
+			# run build
+			Rake.sh ("cmake --build Builds --config " + mode + " -j " + NUM_CPU_CORES)
+
+			puts "\nBuilt plugin QC tools!\n"
+		end
+	end
+end
