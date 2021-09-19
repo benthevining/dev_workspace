e7b7fd96ab1eab17fa3a953f3a35cd668b04b3fa
Ben Vining <ben.the.vining@gmail.com>
2021-09-19 02:41:07 -0500

    updating

--- shell/ci_build.sh
+++ shell/ci_build.sh
@@ -23,6 +23,11 @@ display_and_execute_rake_command() {
 	printf "\n\n rake $1"
 	printf "\n\n"
 	rake $1
+
+	if [ "$?" -ne "0" ]; then
+		echo "Command failed: $1"
+	  	exit 1
+	fi
 }
 
 display_and_execute_rake_command "init"
--- shell/ci_ios_build.sh
+++ shell/ci_ios_build.sh
@@ -6,4 +6,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 bash $SCRIPT_DIR/ci_build.sh
 
+if [ "$?" -ne "0" ]; then
+	echo "Build script failed"
+  	exit 1
+fi
+
 exit 0
