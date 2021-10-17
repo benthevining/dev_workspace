69bbf69529edaf63b9d94b32b6a48993ad4274d1
Ben Vining <ben.the.vining@gmail.com>
2021-10-16 19:18:25 -0500

    Update ci_build.sh

--- shell/ci_build.sh
+++ shell/ci_build.sh
@@ -8,38 +8,16 @@ readonly build_target=${BV_BUILD_TARGET:-all}
 export LEMONS_USE_LV2_JUCE="TRUE"
 export LEMONS_COPY_TO_DEPLOY_FOLDER="TRUE"
 
-export BV_COMMIT_TO_REPOS="FALSE"
-export BV_DEFAULT_BUILD_CONFIG=$build_config
-export BV_DEBUG_RAKE_OUTPUT="FALSE"
-export BV_BUILD_EXTRAS="FALSE"
-
 #
 
 readonly script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-#
-
-readonly deps_script="$script_dir/../Lemons/scripts/install_deps.sh"
-
-chmod u+x "$deps_script"
-bash "$deps_script"
+/usr/bin/env bash "$script_dir/../Lemons/install_deps/install_deps.sh"
 
 #
 
 cd "$script_dir/.."
 
-display_and_execute_rake_command() {
-	printf "\n\n rake $1"
-	printf "\n\n"
-	rake "$1"
-
-	if [ "$?" -ne "0" ]; then
-		echo "Command failed: $1"
-	  	exit 1
-	fi
-}
-
-display_and_execute_rake_command "config[$build_config]"
-display_and_execute_rake_command "build:$build_target[$build_config]"
+make all
 
 exit 0
